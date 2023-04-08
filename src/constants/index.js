27c32ecb888f17ab2fcb1dc3ae0dc4f96f941c4a
abethel912 <abethel180@gmail.com>
2023-04-08 19:01:25 -0400

    Header complete

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-import images from './images';
 import meal from '../assets/meal.mp4';
 import data from './data';
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">A traditional deli Cape Cod Deli</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}> </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+    </div>
   </div>
 );
 
