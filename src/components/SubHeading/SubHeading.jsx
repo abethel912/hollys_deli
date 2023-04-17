d807718e2874600f1f3a2266c46fa5f6c4b6fd5e
abethel912 <abethel180@gmail.com>
2023-04-17 14:51:25 -0400

    New avatar and menu item jsx created

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem' }}>
     <p className="p__cormorant">{title}</p>
-    <img src='https://scontent-bos5-1.xx.fbcdn.net/v/t39.30808-6/320654286_670563501370481_6221904556909679212_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=jTaiN3ALX1QAX8-KURh&_nc_ht=scontent-bos5-1.xx&oh=00_AfDEDwJTyHLOrRnhTdsIFLLa3JIN5rqwb_u_8Q9wOzCNLg&oe=643C2C10' alt="spoon_image" className="spoon__img" />
+    <img src='https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/23224/architetto-tramezzino-clipart-xl.png' alt="spoon_image" className="spoon__img" />
   </div>
 );
 
